d0829f458e0c006ecb38f5bef2557d53bbd9a6e6
Rich Young <git@richjyoung.co.uk>
2013-04-04 14:20:53 +0100

    Initial Commit
    C# Serial Log with Discovery code
    LabVIEW Base Station

---
+++ SerialLogWithDiscover/SerialLog/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+
+namespace SerialLogTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerialPort com6 = new SerialPort("COM5", 115200);
+            com6.Open();
+
+            Thread.Sleep(1000);
+            com6.Write("+++");
+            Thread.Sleep(1000);
+
+            string res = com6.ReadTo("\r");
+            Console.WriteLine(res.Length);
+
+            com6.Write("ATDNWUSATCUBE\r");
+            res = com6.ReadTo("\r");
+            Console.WriteLine(res.Length);
+
+            FileStream fstr = new FileStream("log.txt", FileMode.Create, FileAccess.Write);
+
+            while (true)
+            {
+                while (com6.BytesToRead > 0)
+                {
+                    fstr.WriteByte((byte)com6.ReadByte());
+                }
+                fstr.Flush();
+            }
+        }
+    }
+}
